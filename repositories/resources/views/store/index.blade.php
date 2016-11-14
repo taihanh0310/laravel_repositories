7f461fcf85953bc92e23ec5986940213821322fb
Hanh Nguyen Tai <Hanh Nguyen Tai>
2016-11-14 19:44:11 +0700

    store controller

---
+++ repositories/resources/views/store/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Select from {{ count($data) }} genres</div>
+
+                <div class="panel-body">
+                    <ul>
+                        @foreach($data as $genre)
+                        <li> <a href="{{ route('store.genre') }}">{{ $genre->name }} </a></li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
